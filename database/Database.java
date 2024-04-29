74f8f183401326549619188120c31f39a03f34bf
Will Rages <rageswill@pleasval.org>
2024-04-29 13:56:29 -0500

    Finish database

--- database/Database.java
+++ database/Database.java
@@ -1,18 +1,37 @@
 package database;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Wraps a CSV file with accessor methods.
  */
 public class Database {
-    public final double minimumWage = 14.0;
-    public final HashMap<String, Row> rows;
+    public final String[] headers;
+    public final HashMap<String, String[]> rows = new HashMap<>();
 
     public Database(String fileName) {
-        this.rows = null;
+        File file = new File(fileName);
+        try (var reader = new BufferedReader(new FileReader(file))) {
+            this.headers = reader.readLine().split(",");
+            while (reader.ready()) {
+                String[] cols = reader.readLine().split(",");
+                this.rows.put(cols[0], cols);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     public Row getRow(String primaryKey) {
-        return null;
+        HashMap<String, String> map = new HashMap<>(this.headers.length);
+        final String[] row = rows.get(primaryKey);
+
+        for (int i = 0; i < this.headers.length; i++) {
+            map.put(headers[i], row[i]);
+        }
+
+        return new Row(map);
     }
 }
