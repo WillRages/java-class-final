305bbffea23d23aa0cf4a7f9819c8c00668ee0bc
GhostLucasGames <150816040+GhostLucasGames@users.noreply.github.com>
2024-04-29 13:41:58 -0500

    Update Database.java

--- database/Database.java
+++ database/Database.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
  * Wraps a CSV file with accessor methods.
  */
 public class Database {
+    public final double minimumWage = 14.0;
     public final HashMap<String, Row> rows;
 
     public Database(String fileName) {
