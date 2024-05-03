917d4ed1a4d4c5fb5d172b2799c668b8b1223f53
Will Rages <rageswill@pleasval.org>
2024-05-03 13:25:49 -0500

    More PaneWrapper stuff

--- PaneWrapper.java
+++ PaneWrapper.java
@@ -9,8 +9,26 @@ public class PaneWrapper {
         JOptionPane.showMessageDialog(null, message);
     }
 
-    public static void input(String prompt) {
-        JOptionPane.showInputDialog(null, prompt);
+    public static String inputString(String prompt) {
+        return JOptionPane.showInputDialog(null, prompt);
+    }
+
+    public static Double inputDouble(String prompt) {
+        while (true) try {
+            String input = JOptionPane.showInputDialog(null, prompt);
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            say("Error: " + e);
+        }
+    }
+
+    public static Integer inputInt(String prompt) {
+        while (true) try {
+            String input = JOptionPane.showInputDialog(null, prompt);
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            say("Error: " + e);
+        }
     }
 
     public static void dropdown(String prompt, String defaultValue, String... options) {
