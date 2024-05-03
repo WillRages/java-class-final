5809187de047aec9d5a12fcf2f5c8ed42b9d627d
CdsmArthur <blasquesarthur8@gmail.com>
2024-05-03 13:53:18 -0500

    Brought code to the right file
    Employee --> Manager

--- HamburgerHelperEmployee.java
+++ HamburgerHelperEmployee.java
@@ -1,20 +1,5 @@
 import database.Database;
 
 public class HamburgerHelperEmployee {
-	public static Database database = new Database("assets/data.csv");
-
-    public static void addEmployee() {
-        String name = PaneWrapper.inputString("Enter employee first name");
-        String role = PaneWrapper.inputString("Enter employee role");
-        Integer wage = PaneWrapper.inputInt("Enter employee wage");
-
-        database.addRow(name, role, wage.toString());
-    }
-
-    public static void main(String[] args) {
-        while (PaneWrapper.checkbox("Add employee?"))
-            addEmployee();
-
-        database.writeToFile();
-    }
+	
 }
--- HamburgerHelperManager.java
+++ HamburgerHelperManager.java
@@ -1,3 +1,18 @@
 public class HamburgerHelperManager {
-	
+	public static Database database = new Database("assets/data.csv");
+
+    public static void addEmployee() {
+        String name = PaneWrapper.inputString("Enter employee first name");
+        String role = PaneWrapper.inputString("Enter employee role");
+        Integer wage = PaneWrapper.inputInt("Enter employee wage");
+
+        database.addRow(name, role, wage.toString());
+    }
+
+    public static void main(String[] args) {
+        while (PaneWrapper.checkbox("Add employee?"))
+            addEmployee();
+
+        database.writeToFile();
+    }
 }
