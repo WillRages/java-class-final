860bb056f4348b42cbdd589f0d3cccce1b07aac3
Will Rages <rageswill@pleasval.org>
2024-05-03 13:33:24 -0500

    Add windowName property

--- PaneWrapper.java
+++ PaneWrapper.java
@@ -5,29 +5,35 @@ import javax.swing.*;
  * */
  
 public class PaneWrapper {
+    public static String windowName = "";
+
     public static void say(String message) {
-        JOptionPane.showMessageDialog(null, message);
+        JOptionPane.showMessageDialog(null, message, windowName, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void err(String message) {
+        JOptionPane.showMessageDialog(null, message, windowName, JOptionPane.ERROR_MESSAGE);
     }
 
     public static String inputString(String prompt) {
-        return JOptionPane.showInputDialog(null, prompt);
+        return JOptionPane.showInputDialog(null, prompt, windowName, JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static Double inputDouble(String prompt) {
         while (true) try {
-            String input = JOptionPane.showInputDialog(null, prompt);
+            String input = inputString(prompt);
             return Double.parseDouble(input);
         } catch (NumberFormatException e) {
-            say("Error: " + e);
+            err("Error: " + e);
         }
     }
 
     public static Integer inputInt(String prompt) {
         while (true) try {
-            String input = JOptionPane.showInputDialog(null, prompt);
+            String input = inputString(prompt);
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            say("Error: " + e);
+            err("Error: " + e);
         }
     }
 
