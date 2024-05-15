c28c75ab73c38865ad76daf04d3a8ac31e69ded5
CdsmArthur <blasquesarthur8@gmail.com>
2024-05-14 22:09:19 -0500

    Added tab to add orders and to see employees

--- HamburgerHelperEmployee.java
+++ HamburgerHelperEmployee.java
@@ -1,9 +1,12 @@
 import database.Database;
 import ui.PaneWrapper;
+import ui.MultiForm;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
+import java.util.Arrays;
+import java.awt.event.WindowEvent;
 
 public class HamburgerHelperEmployee { // class start
 
@@ -11,7 +14,20 @@ public class HamburgerHelperEmployee { // class start
 	public static Database inventory = new Database("assets/inventory.csv");
 	public static Database orders = new Database("assets/orders.csv");
 
-	
+	public static JPanel addOrder() {
+		var form = new MultiForm(s -> {
+			orders.addRow(s);
+			PaneWrapper.say(Arrays.toString(s));
+		});
+
+		form.addInput("Order: ", PaneWrapper.makeStringField(""));
+		form.addInput("Address: ", PaneWrapper.makeStringField(""));
+		form.addInput("Price: ", PaneWrapper.makeIntField(0));
+
+		form.addButtons();
+
+		return form;
+	}
 
 	public static JPanel pinTop(JPanel panel) { // pinTop start
 		var wrapper = new JPanel(new BorderLayout());
@@ -24,9 +40,9 @@ public class HamburgerHelperEmployee { // class start
 
 		JTabbedPane pane = new JTabbedPane();
 		pane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-		pane.addTab("Add Item", pinTop(HamburgerHelperManager.addItemMenu()));
+		pane.addTab("Add Orders", pinTop(addOrder()));
 		pane.addTab("View Orders", PaneWrapper.getFromDatabase(orders));
-
+		pane.addTab("View Employees", PaneWrapper.getFromDatabase(employees));
 		frame.getContentPane().add(pane);
 		frame.setPreferredSize(new Dimension(640, 480));
 		frame.setLocationRelativeTo(null);
@@ -39,6 +55,6 @@ public class HamburgerHelperEmployee { // class start
 			public void windowClosing(WindowEvent e) {
 				inventory.writeToFile();
 			}
-		}
+		});
 	} // main end
 } // class end
